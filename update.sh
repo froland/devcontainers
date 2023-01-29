3d421c0a9a2e94143defa85926360469b5589d81
Francois Roland <froland@users.noreply.github.com>
2023-01-29 12:19:03 +0000

    Don't rely on gh cli.

--- update.sh
+++ update.sh
@@ -4,28 +4,28 @@ target_location=$(pwd)
 variant=$1
 
 # Checks if previous file exists
-if [ -f /workspaces/previous_checksum.txt ]; then
-    # If it does, it reads the previous checksum
-    previous_checksum=$(cat /workspaces/previous_checksum.txt)
+if [ -f /workspaces/previous_timestamp.txt ]; then
+    # If it does, it reads the previous timestamp
+    previous_timestamp=$(cat /workspaces/previous_timestamp.txt)
 else
     # If it doesn't, it creates a new file
-    touch /workspaces/previous_checksum.txt
-    # And sets the previous checksum to 0
-    previous_checksum=0
+    touch /workspaces/previous_timestamp.txt
+    # And sets the previous timestamp to 0
+    previous_timestamp=0
 fi
 
-# get latest release checksum
-current_checksum=$(gh release view --json 'assets[0].checksum' --repo froland/devcontainers)
+# get latest release timestamp
+current_timestamp=$(curl -s https://api.github.com/repos/froland/devcontainers/releases/latest | jq -r '.created_at')
 
-# Checks if the checksums are different
-if [ "$previous_checksum" != "$current_checksum" ]; then
+# Checks if the timestamps are different
+if [ "$previous_timestamp" != "$current_timestamp" ]; then
     # If they are, it downloads the latest release
-    gh release download -O /workspaces/devcontainers_latest.tar.gz --archive=tar.gz --clobber --repo froland/devcontainers
-    # And updates the previous checksum
-    echo "$current_checksum" > /workspaces/previous_checksum.txt
+    wget -O /workspaces/devcontainers_latest.tar.gz https://github.com/froland/devcontainers/releases/latest
+    # And updates the previous timestamp
+    echo "$current_timestamp" > /workspaces/previous_timestamp.txt
 
     # Unpacks the release in temporary folder
-    mkdir /workspaces/devcontainers_temp
+    mkdir -p /workspaces/devcontainers_temp
     tar -xzf /workspaces/devcontainers_latest.tar.gz -C /workspaces/devcontainers_temp
 
     # Refresh refresh script
