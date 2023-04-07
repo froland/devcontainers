7bff9f32a32f1e0b1bae5cd1af8b09df0b84ab68
Francois Roland <froland@froland.be>
2023-04-07 19:17:01 +0200

    Tune VNC server.

--- src/amc/.devcontainer/entrypoint.sh
+++ src/amc/.devcontainer/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-x11vnc -create -gone 'killall Xvfb' -nevershared -forever -loop -nopw -localhost -no6 &
-fluxbox &
-auto-multiple-choice &
+x11vnc -xdummy \
+  -env FD_PROG=/usr/bin/fluxbox \
+  -env X11VNC_FINDDISPLAY_ALWAYS_FAIL=1 \
+  -env X11VNC_CREATE_GEOM=1920x1080 \
+  -no6 -noipv6 \
+  -shared -forever -loop -nopw -tightfilexfer -xrandr &
+/home/vscode/noVNC-1.4.0/utils/novnc_proxy --listen 6900 --vnc localhost:5900 &
 
 exec "$@"
