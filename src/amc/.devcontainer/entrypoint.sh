2773b9f0a800be11a850e964b871b21db6a080e3
François Roland <froland@users.noreply.github.com>
2023-04-07 13:53:06 +0200

    Ensure Dockerfile commands are run when used as devcontainer.

--- src/amc/.devcontainer/entrypoint.sh
+++ src/amc/.devcontainer/entrypoint.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
 x11vnc -create -gone 'killall Xvfb' -nevershared -forever -loop -nopw -localhost -no6 &
 fluxbox &
-auto-multiple-choice
+auto-multiple-choice &
+
+exec "$@"
